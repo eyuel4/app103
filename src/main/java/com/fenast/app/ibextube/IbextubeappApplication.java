b99e14301f61dd0a49db96e9c596f16cda021857
taddesee <eyuel_taddese@wgresorts.com>
2018-02-08 17:13:17 -0500

    started working on ResourceServer Oauth2

---
+++ src/main/java/com/fenast/app/ibextube/IbextubeappApplication.java
@@ -0,0 +1,13 @@
+package com.fenast.app.ibextube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class IbextubeappApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbextubeappApplication.class, args);
+	}
+}
---
+++ src/main/java/com/fenast/app/ibextube/config/oauth2/CustomAccessTokenConverter.java
@@ -0,0 +1,23 @@
+package com.fenast.app.ibextube.config.oauth2;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Created by taddesee on 2/8/2018.
+ */
+
+@Component
+public class CustomAccessTokenConverter extends DefaultAccessTokenConverter {
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
+        OAuth2Authentication authentication = super.extractAuthentication(claims);
+        authentication.setDetails(claims);
+        return authentication;
+    }
+
+}
