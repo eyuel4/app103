360ea1af51133be5216c4f7b2bcd8de43f2a7009
taddesee <eyuel_taddese@wgresorts.com>
2018-02-23 12:21:47 -0500

    Added Movie Model and Configure swagger

---
+++ src/main/java/com/fenast/app/ibextube/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.fenast.app.ibextube.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by taddesee on 2/23/2018.
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@EnableSwagger2
+@ComponentScan("com.fenast.app.ibextube")
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
